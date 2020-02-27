68c40b671659f4790a7dad30bc16235bb1f31995
Leonardo Robol <leo@robol.it>
2020-02-27 08:46:49 +0100

    Adjusted example1.

--- examples/example1.f90
+++ examples/example1.f90
@@ -1,9 +1,10 @@
-! Example 1: Computing the roots of a smooth function in [-1, 1]
+! Example 1: Computing the roots of a smooth function in [-1, 1],
+! for which exact roots are known. 
 
 function f(x)
   implicit none
   double precision :: f, x
-  f = dsin(100 * x);
+  f = dsin(400 * x);
 end function f
 
 program example1
@@ -11,7 +12,7 @@ program example1
 
   integer j, n
   double precision, allocatable :: zeros(:)
-  double precision :: y
+  double precision :: y, PI = 4.d0 * atan(1.d0), tol = 1.d-12
 
   interface
      function f(x)
@@ -19,14 +20,24 @@ program example1
      end function f
   end interface
 
+  ! This is needed to have the interface for calling cqr_zeros,
+  ! which allocates the vector zeros(:). 
   INCLUDE '../chebqr.h'
   
-  call cqr_zeros(f, 1.d-12, zeros, n)
+  call cqr_zeros(f, tol, zeros, n)
 
-  print *, 'Number of roots in [-1, 1]:', n
+  print '(A)', 'Function: sin(400*x) over [-1, 1]'
+  print '(A, E8.3)', 'Approximation tolerance: ', tol
+  print '(A, I4)', 'Number of expected roots: ', floor(2 * 400 / PI + 1)
+  print '(A, I4)', 'Number of identified roots in [-1, 1]:', n
+  print *, ''
 
   do j = 1, n
-     print *, 'root #', j, ' = ', zeros(j), 'f(root) =', f(zeros(j))
+     print '(A, I3, A, F8.5, A, E10.3, A, F8.5, A, E10.3)', &
+          'root #', j, ' = ', zeros(j), &
+          ' | f(root) = ', f(zeros(j)), &
+          ' | exact root = ', PI * (j - n/2 - 1) / 400, &
+          ' | error = ', abs(zeros(j) - PI * (j - n/2 - 1) / 400)
   end do
 
   deallocate(zeros)
